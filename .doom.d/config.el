d130a26d442c6211587c9c1bbf0597e933bdfbdf
Aldi Daswanto <aldi@daswanto.com>
2022-03-10 21:38:08 +0700

    clean-up

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,68 +1,20 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Aldi Daswanto"
       user-mail-address "aldi@daswanto.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "VictorMono Nerd Font" :size 15 :style "Reg-Patched" :weight 'normal :height 120 :style "Light")
-;;        doom-variable-pitch-font (font-spec :family "Lato" :size 13 :weight 'light))
-
-;; (setq doom-font (font-spec :family "Victor Mono 1.4" :size 15 :style "Regular 1.4" :weight 'normal :height 120)
-;;       doom-variable-pitch-font (font-spec :family "Lato" :style "Light" :size 13 :weight 'normal)
-;;       doom-serif-font (font-spec :family "Victor Mono 1.4" :size 15 :style "Italic 1.4" :weight 'normal :height 120))
 
 (setq doom-font (font-spec :family "Victor Mono 1.4" :size 13 :style "Regular 1.4" :weight 'normal :height 130)
       doom-variable-pitch-font (font-spec :family "Lato" :style "Light" :size 11 :weight 'normal)
       doom-serif-font (font-spec :family "Victor Mono 1.4" :size 13 :style "Italic 1.4" :weight 'normal :height 130))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (load-theme 'doom-dracula t)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 (setq org-log-done 'time)
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
 (solaire-global-mode -1)
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-;(set-face-attribute 'default nil :height 150)
-;
 (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.njk\\'" . web-mode))
 (add-to-list 'default-frame-alist '(height . 30))
